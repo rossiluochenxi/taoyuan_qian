1c3c74ff0d619a61918aa4c6c780f76a45613dea
罗晨熙 <15164241340@avic-d.com>
2024-03-17 19:52:38 +0800

    提交

--- ruoyi-ui/src/views/dm/rtdata/index.vue
+++ ruoyi-ui/src/views/dm/rtdata/index.vue
@@ -12,7 +12,7 @@
       <el-form-item label="设备编号" prop="agroLivestockIccid">
         <el-input
           v-model="queryParams.agroLivestockIccid"
-          placeholder="请输入设备编号1111122asdsa"
+          placeholder="请输入设备编号1111122asdsa12"
           clearable
           @keyup.enter.native="handleQuery"
         />
