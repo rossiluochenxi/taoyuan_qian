470a42105c2bb42b9734413f1ac441abd8e40148
罗晨熙 <15164241340@avic-d.com>
2024-04-22 16:03:46 +0800

    地图

--- ruoyi-ui/src/views/datamonitor/VidSurveillance/index.vue
+++ ruoyi-ui/src/views/datamonitor/VidSurveillance/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    <img src="@/assets/images/jk2.jpg" alt="Example Image">
+  <div @click="startPlayback">
+    <video ref="videoPlayer" controls></video>
   </div>
 </template>
 
 <script>
+import flvjs from 'flv.js';
+
 export default {
-  name: 'ImageComponent'
+  methods: {
+    startPlayback() {
+      if (flvjs.isSupported()) {
+        const videoElement = this.$refs.videoPlayer;
+        const flvPlayer = flvjs.createPlayer({
+          type: 'flv',
+          url: 'rtmp://127.0.0.1/live/test' // 替换为你的 RTMP 视频流地址
+        });
+        flvPlayer.attachMediaElement(videoElement);
+        flvPlayer.load();
+        flvPlayer.play();
+      }
+    }
+  }
 }
 </script>
-
-<style scoped>
-/* 在这里可以添加样式 */
-</style>
